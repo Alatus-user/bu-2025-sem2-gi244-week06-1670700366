41013e72c6645133d952e7099c27dbdd1f61e9b0
Alatus-user <thirasak26679@gmail.com>
2026-02-20 16:37:55 +0700

    LabExtermination_Week-06
    ทำได้แล้วครับ เพราะทำกัน 4 คน

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -20,4 +20,6 @@ public class DestroyOutOfBound : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    
 }
--- Assets/Scripts/Exam/CameraControlExam06.cs
+++ Assets/Scripts/Exam/CameraControlExam06.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraControlExam06 : MonoBehaviour
@@ -6,6 +7,7 @@ public class CameraControlExam06 : MonoBehaviour
     public GameObject player2;
     public float offset;
     public Camera targetCamera;
+    public Vector3 middlePoint;
 
     // Update is called once per frame
     void LateUpdate()
@@ -13,6 +15,36 @@ public class CameraControlExam06 : MonoBehaviour
         Vector3 player1Pos = player1.transform.position;
         Vector3 player2Pos = player2.transform.position;
 
-        // Student code ...
+        middlePoint = (player1Pos + player2Pos) * 0.5f;
+        
+        targetCamera.transform.position = middlePoint + new Vector3(0, 40, offset);
+
+        if (targetCamera != null)
+
+        {
+
+            if (targetCamera.orthographic)
+
+            {
+
+                float distance = Vector3.Distance(player1Pos, player2Pos);
+                float aspectRatio = targetCamera.aspect;
+                targetCamera.orthographicSize = Mathf.Max(distance * 0.5f, distance / (2 * aspectRatio)) + 10f;
+            }
+
+        }
+
+            else
+
+            {
+
+                float distance = Vector3.Distance(player1Pos, player2Pos);
+
+                targetCamera.fieldOfView = Mathf.Lerp(60f, 90f, distance / 20f);
+
+            }
+
+        }
+
     }
-}
+
